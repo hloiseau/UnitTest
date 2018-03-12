870fcf2875f8db13a28af7cbb4a030540b52bb74
hloiseau <32191873+hloiseau@users.noreply.github.com>
2018-03-12 10:49:38 +0100

    Unit Test made by Antoine

---
+++ ConsoleApp2/BddTests/SimpleTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BddTests
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        [Test]
+        public void should_pass()
+        {
+
+        }
+
+        [Test]
+        public void should_fail()
+        {
+            Assert.That(4, Is.EqualTo(0));
+        }
+    }
+}
